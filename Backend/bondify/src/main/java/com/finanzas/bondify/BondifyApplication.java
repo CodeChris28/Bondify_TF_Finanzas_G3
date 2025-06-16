ef35b881ff4684273885d8eb73b96717025a01f5
Christian <u202123934@upc.edu.pe>
2025-06-15 23:21:43 -0500

    springboot starter

---
+++ Backend/bondify/src/main/java/com/finanzas/bondify/BondifyApplication.java
@@ -0,0 +1,13 @@
+package com.finanzas.bondify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondifyApplication.class, args);
+	}
+
+}
---
+++ Backend/bondify/src/test/java/com/finanzas/bondify/BondifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzas.bondify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
