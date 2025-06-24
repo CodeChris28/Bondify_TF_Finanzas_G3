b0af2d06588c52b061c82846e62ae52f000295a5
Vanessa471424 <u20221d526@upc.edu.pe>
2025-06-24 17:54:55 -0500

    completo BonOperation

--- Backend/bondify/src/main/java/com/finanzas/bondify/servicesimplements/BondOperationServiceImplement.java
+++ Backend/bondify/src/main/java/com/finanzas/bondify/servicesimplements/BondOperationServiceImplement.java
@@ -1,4 +1,39 @@
 package com.finanzas.bondify.servicesimplements;
 
-public class BondOperationServiceImplement {
+import com.finanzas.bondify.entities.BondOperation;
+import com.finanzas.bondify.repositories.IBondOperationRepository;
+import com.finanzas.bondify.servicesinterfaces.IBondOperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class BondOperationServiceImplement implements IBondOperationService {
+
+    @Autowired
+    private IBondOperationRepository bR;
+
+    @Override
+    public void insert(BondOperation bondOperation) {
+        bondOperation.setCreatedAt(LocalDateTime.now());
+        bondOperation.setUpdatedAt(LocalDateTime.now());
+        bR.save(bondOperation);
+    }
+
+    @Override
+    public List<BondOperation> list() {
+        return bR.findAll();
+    }
+
+    @Override
+    public void delete(int id) {
+        bR.deleteById(id);
+    }
+
+    @Override
+    public BondOperation listId(int id) {
+        return bR.findById(id).orElse(new BondOperation());
+    }
 }
